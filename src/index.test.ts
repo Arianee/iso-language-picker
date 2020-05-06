e1c795c749078b18ec21e6b3b8d3362b20574140
Maxime <maxime@vaugam.com>
2020-05-06 14:55:54 +0200

    feat: change logical and add macro language

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,14 +1,90 @@
-import {pickLanguageAccordingToUserLanguages} from "./index";
+import {pickLanguageAccordingToUserLanguages,pickLanguageAccordingToUserLanguagesWithMacrosFallback} from "./index";
 
-describe('test', () => {
-    test("test1", () => {
-        expect(true).toBeTruthy();
-    });
-    const testSet = [
+describe("macro languages",()=>{
+    const testSet=[ 
         {
-            userLanguages: ['es', 'en-US'],
-            availableLanguages: ['fr', 'en-US'],
+            macros:['fr-FR', 'ja-JP', 'en-US', 'ko-KR', 'de-DE'],
+            userLanguages: ['fr-BE'],
+            availableLanguages: ['fr-FR'],
+            expectedLanguages: 'fr-FR',
+            defaultLanguage: 'defaultLanguage'
+        },
+        {
+            macros:['fr-FR', 'ja-JP', 'en-US', 'ko-KR', 'de-DE'],
+            userLanguages: ['en-UK'],
+            availableLanguages: ['en-US'],
             expectedLanguages: 'en-US',
+            defaultLanguage: 'defaultLanguage'
+        },
+        {
+            macros:['fr-FR', 'ja-JP', 'en-US', 'ko-KR', 'de-DE'],
+            userLanguages: ['de-AU'],
+            availableLanguages: ['de-DE'],
+            expectedLanguages: 'de-DE',
+            defaultLanguage: 'defaultLanguage'
+        },
+        {
+            macros:['fr-FR', 'ja-JP', 'en-US', 'ko-KR', 'de-DE'],
+            userLanguages: ['de'],
+            availableLanguages: ['de-DE'],
+            expectedLanguages: 'de-DE',
+            defaultLanguage: 'defaultLanguage'
+        },
+        {
+            macros:['fr-FR', 'ja-JP', 'en-US', 'ko-KR', 'de-DE'],
+            userLanguages: ['de-AU','de'],
+            availableLanguages: ['de-DE','de-AU'],
+            expectedLanguages: 'de-AU',
+            defaultLanguage: 'defaultLanguage'
+        },
+        {
+            macros:['fr-FR', 'ja-JP', 'en-US', 'ko-KR', 'de-DE'],
+            userLanguages: ['de-AU','en-US'],
+            availableLanguages: ['de-DE','en-US'],
+            expectedLanguages: 'de-DE',
+            defaultLanguage: 'defaultLanguage'
+        },
+        {
+            macros:['fr-FR', 'ja-JP', 'en-US', 'ko-KR', 'de-DE'],
+            userLanguages: ['de','en-US'],
+            availableLanguages: ['de-DE','en-US'],
+            expectedLanguages: 'de-DE',
+            defaultLanguage: 'defaultLanguage'
+        },
+        {
+            macros:['fr-FR', 'ja-JP', 'en-US', 'ko-KR', 'de-DE'],
+            userLanguages: ['en-UK',"es"],
+            availableLanguages: ['en-US','Ka-BE'],
+            expectedLanguages: 'en-US',
+            defaultLanguage: 'defaultLanguage'
+        },
+        {
+            macros:['fr-FR', 'ja-JP', 'en-US', 'ko-KR', 'de-DE'],
+            userLanguages: ['en-UK',"es"],
+            availableLanguages: ['en-US','Ka-BE','es'],
+            expectedLanguages: 'en-US',
+            defaultLanguage: 'defaultLanguage'
+        }
+    ]
+
+
+    testSet.forEach((data,index)=>{
+        const {userLanguages,availableLanguages,expectedLanguages,defaultLanguage,macros}=data;
+
+        test(`${index}/ macros: ${macros}, user languages: ${userLanguages}, availables: ${availableLanguages}, default: ${defaultLanguage}, expected: ${expectedLanguages}`,()=>{
+            const result = pickLanguageAccordingToUserLanguagesWithMacrosFallback(macros, userLanguages, availableLanguages, defaultLanguage);
+
+            expect(result).toBe(expectedLanguages)
+        })
+    })
+})
+
+describe('normal behavior', () => {
+    const testSet = [
+       {
+            userLanguages: ['de-AU', 'en-US'],
+            availableLanguages: ['de', 'en-US'],
+            expectedLanguages: 'de',
             defaultLanguage: 'defaultLanguage'
         },
         {
@@ -20,7 +96,7 @@ describe('test', () => {
         {
             userLanguages:  ['es', 'ch-MO'],
             availableLanguages: ['fr', 'en-UK', 'ch-TW'],
-            expectedLanguages: 'ch-TW',
+            expectedLanguages: 'defaultLanguage',
             defaultLanguage: 'defaultLanguage'
         },
         {
@@ -29,34 +105,34 @@ describe('test', () => {
             expectedLanguages: 'ja',
             defaultLanguage: 'defaultLanguage'
         },
-        {
+       {
             userLanguages: ['ja', 'en-US'],
             availableLanguages: ['ja-JP', 'en'],
-            expectedLanguages: 'ja-JP',
+            expectedLanguages: 'en',
             defaultLanguage: 'defaultLanguage'
         },
         {
             userLanguages: ['fr-FR', 'fr-BE'],
             availableLanguages: ['fr-KA'],
-            expectedLanguages: 'fr-KA',
+            expectedLanguages: 'defaultLanguage',
             defaultLanguage: 'defaultLanguage'
         },
         {
             userLanguages: [ 'fr-BE'],
             availableLanguages: ['fr-FR'],
-            expectedLanguages: 'fr-FR',
+            expectedLanguages: 'defaultLanguage',
             defaultLanguage: 'defaultLanguage'
         },
         {
             userLanguages: [ 'en-UK'],
             availableLanguages: ['en-US'],
-            expectedLanguages: 'en-US',
+            expectedLanguages: 'defaultLanguage',
             defaultLanguage: 'defaultLanguage'
         },
         {
             userLanguages: [ 'zh-mo'],
             availableLanguages: ['zh-CN'],
-            expectedLanguages: 'zh-CN',
+            expectedLanguages: 'defaultLanguage',
             defaultLanguage: 'defaultLanguage'
         },
     ];
